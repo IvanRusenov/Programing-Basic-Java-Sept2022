27ddf6d6e00b5de03bfafa3dda64aea6624d1361
IvanRusenov <i_rusenov@abv.bg>
2022-09-27 17:41:26 +0200

    Create projectsCreation.java

---
+++ firstStepsInCodingLab/projectsCreation.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class projectsCreation {
+
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        int projectCount = Integer.parseInt(scan.nextLine());
+
+        int projectsTime = projectCount * 3;
+
+        System.out.printf("The architect %s " +
+                "will need %d hours to complete %d project/s.",name,projectsTime,projectCount);
+
+
+
+    }
+}
