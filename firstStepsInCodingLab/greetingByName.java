8e3f5464b15fc424ddebd303c6957277f165a15d
IvanRusenov <i_rusenov@abv.bg>
2022-09-28 08:38:49 +0200

    Create greetingByName.java

---
+++ firstStepsInCodingLab/greetingByName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class greetingByName {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        String name = scan.nextLine();
+
+        System.out.println("Hello " + name +"!");
+
+
+    }
+}
