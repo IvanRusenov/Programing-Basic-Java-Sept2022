9eb47907aee93ae2d5add20ed8f9028a1d3ecc21
IvanRusenov <i_rusenov@abv.bg>
2022-09-30 11:24:43 +0200

    Create vacantionBooks.java

---
+++ FirstStepsInCodingExercise/vacantionBooks.java
@@ -0,0 +1,21 @@
+package FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class vacantionBooks {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int pagesCount = Integer.parseInt(scan.nextLine());
+        int pagesPerHour = Integer.parseInt(scan.nextLine());
+        int days = Integer.parseInt(scan.nextLine());
+
+        int timeToRead = pagesCount / pagesPerHour;
+        int neededDays = timeToRead / days;
+
+        System.out.println(neededDays);
+
+
+    }
+}
