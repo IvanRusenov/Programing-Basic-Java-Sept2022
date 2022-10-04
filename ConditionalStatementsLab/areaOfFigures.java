ad29ca7d436f7275212aee741bce408c7e9908de
IvanRusenov <i_rusenov@abv.bg>
2022-10-04 05:39:27 +0200

    Create areaOfFigures.java

---
+++ ConditionalStatementsLab/areaOfFigures.java
@@ -0,0 +1,33 @@
+package conditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class areaOfFigures {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String figure = scan.nextLine();
+        double area = 0;
+
+        if (figure.equals("square")){
+            double a = Double.parseDouble(scan.nextLine());
+            area = a * a;
+        }else if (figure.equals("rectangle")) {
+            double a = Double.parseDouble(scan.nextLine());
+            double b = Double.parseDouble(scan.nextLine());
+            area = a * b;
+        }else if(figure.equals("circle")) {
+            double a = Double.parseDouble(scan.nextLine());
+            area = Math.PI * a * a;
+        }else if(figure.equals("triangle")){
+            double a = Double.parseDouble(scan.nextLine());
+            double h = Double.parseDouble(scan.nextLine());
+            area = a * h / 2;
+        }
+
+        System.out.printf("%.3f",area);
+
+
+    }
+}
