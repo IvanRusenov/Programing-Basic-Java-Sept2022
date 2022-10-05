49de65f6a5bd57ba463dfa6e0748d8e7fd7f9508
IvanRusenov <i_rusenov@abv.bg>
2022-10-05 09:26:25 +0200

    Create bonusScore.java

---
+++ conditionalStatementsExercise/bonusScore.java
@@ -0,0 +1,32 @@
+package conditionalStatementsExercise;
+
+import java.util.Scanner;
+
+public class bonusScore {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num = Integer.parseInt(scan.nextLine());
+        double bonus = 0.0;
+
+        if (num <= 100){
+            bonus = bonus + 5;
+        }else if (num <= 1000){
+            bonus = num * 0.20;
+        }else {
+            bonus = num * 0.10;
+        }
+
+        if(num%2 == 0){
+            bonus = bonus +1;
+        }else if (num % 10 == 5){
+            bonus = bonus + 2;
+        }
+
+        double sum = bonus + num;
+        System.out.println(bonus);
+        System.out.println(sum);
+
+    }
+}
