6c407f86c6ec4b28e4ae42769b35fd799183711a
IvanRusenov <i_rusenov@abv.bg>
2022-10-24 11:37:58 +0200

    Create minNumber.java

---
+++ whileLoopLab/minNumber.java
@@ -0,0 +1,23 @@
+package whileLoopLab;
+
+import java.util.Scanner;
+
+public class minNumber {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        int minNum = Integer.MAX_VALUE;
+
+        while (!(input.equals("Stop"))) {
+            int num = Integer.parseInt(input);
+            if (num<minNum){
+                minNum = num;
+            }
+            input = scan.nextLine();
+        }
+        System.out.println(minNum);
+
+    }
+}
