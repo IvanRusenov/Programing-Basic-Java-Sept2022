c17f9debb5a6a327a8d292a991716481497bec08
IvanRusenov <i_rusenov@abv.bg>
2022-10-24 11:37:21 +0200

    Create maxNumber.java

---
+++ whileLoopLab/maxNumber.java
@@ -0,0 +1,23 @@
+package whileLoopLab;
+
+import java.util.Scanner;
+
+public class maxNumber {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        int maxNum = Integer.MIN_VALUE;
+
+        while(!(input.equals("Stop"))){
+            int num  = Integer.parseInt(input);
+
+            if (num>maxNum){
+                maxNum = num;
+            }
+            input = scan.nextLine();
+        }
+        System.out.println(maxNum);
+    }
+}
