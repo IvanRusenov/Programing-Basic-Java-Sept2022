6cfc2b4554c36bfc37d8ccd17fd53669b3fef501
IvanRusenov <i_rusenov@abv.bg>
2022-12-06 08:57:00 +0200

    Create task2.java

---
+++ realExam22october22/task2.java
@@ -0,0 +1,11 @@
+package realExam22october22;
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
